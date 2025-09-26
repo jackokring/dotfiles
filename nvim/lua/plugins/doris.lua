3d79208590aeec89d386df2b6cbc88be33cfd6e7
jackokring <jackokring@gmail.com>
2025-09-26 12:00:07 +0000

    nvim

---
+++ nvim/lua/plugins/doris.lua
@@ -0,0 +1,32 @@
+--return nil
+-- doris plugin loader for nvim
+return {
+  "jackokring/doris.nvim",
+  -- **local build**
+  dev = true,
+  dir = "~/projects/doris.nvim",
+  fallback = true,
+  -- **build command**
+  build = "./build.sh",
+  -- **setup options**
+  opts = {},
+  -- **lazy load info**
+  -- lazy = true,
+  -- **on event**
+  -- event = { "BufEnter", "BufEnter *.lua" },
+  -- **on command use**
+  -- cmd = { "cmd" },
+  -- **on filetype**
+  -- ft = { "lua" },
+  -- **on keys**
+  -- keys = {
+  --    -- key tables
+  --    {
+  --      "<leader>ft",
+  --      -- "<cmd>Neotree toggle<cr>",
+  --      -- desc = "NeoTree",
+  --      -- mode = "n",
+  --      -- ft = "lua"
+  --    },
+  --  },
+}
